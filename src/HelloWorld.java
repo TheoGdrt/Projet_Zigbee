4a3c082f0ed1fed3fb9f9c26761ac58d704cb744
Théo Godart <godart.theo@gmail.com>
2019-10-25 10:57:11 +0200

    first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ import com.rapplogic.xbee.api.XBeeAddress64;
 import com.rapplogic.xbee.api.XBeeException;
 import com.rapplogic.xbee.api.XBeeRequest;
 import com.rapplogic.xbee.api.XBeeResponse;
+import com.rapplogic.xbee.api.zigbee.ZNetRxIoSampleResponse;
 import com.rapplogic.xbee.util.ByteUtils;
 import com.rapplogic.xbee.api.PacketListener;
 import com.rapplogic.xbee.api.RemoteAtRequest;
@@ -15,19 +16,34 @@ import com.rapplogic.xbee.api.RemoteAtRequest;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
         
         XBee xbee = new XBee();
         try {
-			xbee.open("COM6", 9600);
+			xbee.open("COM6", 9600); //connexion à un port com
 			
-			 XBeeAddress64 remoteAddress = new XBeeAddress64(0, 0, 0, 0, 0, 0, 0, 0);
-		        RemoteAtRequest request = new RemoteAtRequest(XBeeRequest.DEFAULT_FRAME_ID, remoteAddress, XBeeAddress16.BROADCAST, false, "NI");
-		        xbee.sendAsynchronous(request);
+			 XBeeAddress64 remoteAddress = new XBeeAddress64(0, 0, 0, 0, 0, 0, 0, 0); //à qui je veux parler
+		        RemoteAtRequest request = new RemoteAtRequest(XBeeRequest.DEFAULT_FRAME_ID, remoteAddress, XBeeAddress16.BROADCAST, false, "NI"); //création de la requête
+		        xbee.sendAsynchronous(request); //envoi de la requête
 		        
-		        xbee.addPacketListener(new PacketListener() {
+		        xbee.addPacketListener(new PacketListener() { //écoute de la requête
 		            public void processResponse(XBeeResponse response) {
+		            	//IO_SAMPLE_RESPONSE
+		            	if (response.getApiId() == ApiId.RX_16_RESPONSE) {
+		            		   // since this API ID is AT_RESPONSE, we know to cast to AtCommandResponse
+		            		    ZNetRxIoSampleResponse atResponse = (ZNetRxIoSampleResponse) response;
+
+		            		    if (atResponse.containsAnalog()) {
+		            		        // command was successful
+		            		        System.out.println("Command returned " + ByteUtils.toString(atResponse.getValue()));
+		            		    } else {
+		            		        // command failed!
+		            		    	System.out.println("Command returned ");
+		            		    }
+		            		    
+		            		}
+		            	
+		            	//REMOTE_AT_RESPONSE
 		            	if (response.getApiId() == ApiId.REMOTE_AT_RESPONSE) {
 		            		   // since this API ID is AT_RESPONSE, we know to cast to AtCommandResponse
 		            		    AtCommandResponse atResponse = (AtCommandResponse) response;
@@ -37,7 +53,7 @@ public class HelloWorld {
 		            		        System.out.println("Command returned " + ByteUtils.toString(atResponse.getValue()));
 		            		    } else {
 		            		        // command failed!
-		            		    	System.out.println("Command returned ");
+		            		    	System.out.println("Command returned "); 
 		            		    }
 		            		    
 		            		}
